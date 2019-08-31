d58a1d650d7075e743f34865839f94ab5fe7ff1c
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-31 04:03:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,14 +2,12 @@ class Dog
 
 def initialize(name)
   @name = name 
-end 
-
-def name 
-    @name 
-  end 
  
- def initialize(breed) 
   @breed = breed
- end 
+  
+def name
+  @name 
+    end 
+  end 
 end 
   
